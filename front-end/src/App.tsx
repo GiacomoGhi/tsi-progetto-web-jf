b04c5bf6c817a63d696cd0d5186a48818efa5f1b
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2023-12-24 15:19:17 +0100

    added bootstrap

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.scss";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <h1>My app</h1>;
+  return (
+    <>
+      <h1>My app</h1>
+      <Button>button</Button>
+    </>
+  );
 }
 
 export default App;

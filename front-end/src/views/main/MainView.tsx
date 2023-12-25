f37dcb9c7844211ce3370ffcd0ac1d25680ad3f4
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2023-12-25 17:39:23 +0100

    fixed logo dark mode bug

--- front-end/src/views/main/MainView.tsx
+++ front-end/src/views/main/MainView.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import AppNavigationMenu from './components/app-nav-menu/AppNavigationMenu'
 import AppFooter from './components/footer/AppFooter'
-
+import './MainView.styles.scss'
 function MainView() {
   return (
-    <div id="app">
+    <div className="root" id="app">
       <AppNavigationMenu />
       <main>{/* <router-view></router-view> */}</main>
       <AppFooter />

89e28c5d90e076d083db716354e98fba9221fdf5
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2024-01-05 11:54:21 +0100

    added mail env config to mail module

--- back-end/src/mail/lib/backend-mail.module.ts
+++ back-end/src/mail/lib/backend-mail.module.ts
@@ -3,27 +3,35 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { join } from 'path';
+import { BackendConfigModule, BackendConfigService } from '@config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.example.com',
-        secure: false,
-        auth: {
-          user: 'user@example.com',
-          pass: 'topsecret',
-        },
-      },
-      defaults: {
-        from: '"No Reply" <noreply@example.com>',
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
+    MailerModule.forRootAsync({
+      imports: [BackendConfigModule],
+      inject: [BackendConfigService],
+      useFactory: async (configProvider: BackendConfigService) => {
+        const config = configProvider.getConfiguration();
+        return {
+          transport: {
+            host: config.mailHost,
+            secure: false,
+            auth: {
+              user: config.mailUser,
+              pass: config.mailPassword,
+            },
+          },
+          defaults: {
+            from: `"No Reply" <${config.mailFrom}>`,
+          },
+          template: {
+            dir: join(__dirname, 'templates'),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
       },
     }),
   ],

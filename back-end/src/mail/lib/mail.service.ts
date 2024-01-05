51beba8bf082830e6db2dab096c814f5609c3979
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2024-01-05 11:15:21 +0100

    added mail service

--- back-end/src/mail/lib/mail.service.ts
+++ back-end/src/mail/lib/mail.service.ts
@@ -1,4 +1,23 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from '@services';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendUserConfirmation(user: UserEntity, token: string) {
+    const url = `example.com/auth/confirm?token=${token}`; // TODO add email confermation end point
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      from: '"Support Team" <support@example.com>',
+      subject: 'Welcome to AR & VR App! Confirm your Email',
+      template: './confirmation',
+      context: {
+        name: user.name,
+        url,
+      },
+    });
+  }
+}

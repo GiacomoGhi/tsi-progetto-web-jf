7fae0e1accc6c4c5bec7e9e1317ae758dccb42bd
Giacomo Ghinelli <giacomoghinelli9@gmail.com>
2024-01-08 22:44:08 +0100

    deleted useless imports

--- front-end/src/views/main/components/private-route/PrivateRoute.tsx
+++ front-end/src/views/main/components/private-route/PrivateRoute.tsx
@@ -1,5 +1,4 @@
-import App from 'App'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 
 const PrivateRoute: React.FC<{ isAuth: boolean }> = ({ isAuth }) => {

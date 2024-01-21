ebaedc67242f2310ac1ddfc449682755939e689c
Fabio Tiralongo <fabio.tiralongo@studio.unibo.it>
2024-01-21 22:35:02 +0100

    monogram in footer

--- front-end/src/views/main/components/footer/AppFooter.tsx
+++ front-end/src/views/main/components/footer/AppFooter.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './AppFooter.style.scss'
+import { Monogram } from 'assets'
+
 function AppFooter() {
   return (
     <footer>
-      <div className="container-fluid text-white text-center rounded-top-4 footerStyle">
+      <div className="container-fluid text-white rounded-top-4 footerStyle">
         <div className="row">
-          <div className="col-12">
-            <h3 className="pt-3">Developed By</h3>
-            <ul className="list-unstyled">
-              <li>Giacomo Ghinelli</li>
-              <li className="py-1">Fabio Tiralongo</li>
-            </ul>
+          <div className="col-12 d-flex align-items-center">
+            {/* Logo a sinistra */}
+            <img src={Monogram} alt="Logo" className="footer-logo" />
+
+            <div className="mx-auto text-center">
+              {/* Testo al centro orizzontalmente */}
+              <h3 className="pt-3 footer-text">Developed By</h3>
+              <ul className="list-unstyled footer-text">
+                <li>Giacomo Ghinelli</li>
+                <li>Fabio Tiralongo</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
